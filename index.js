12067c5d897ee9de5fce3bed115fe0588224ea98
gabrielsaintz <gabrielsaintzwmv@gmail.com>
2022-07-13 03:02:14 -0300

    Added express, ejs and transferred .html for .ejs

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/home", (req, res) => {
+   res.render("home");
+});
+
+app.get("/client", (req, res) => {
+   res.render("myaccountclient");
+});
+
+app.get("/pro", (req, res) => {
+   res.render("myaccountpro");
+});
+
+app.listen(PORT, () => console.log("O servidor está funcionando corretamente"));
